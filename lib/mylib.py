2ad5c5dd6c436f3755adfe47ace0bb900e4a5859
zmc <zengmeichunmt@163.com>
2018-07-15 12:13:11 +0800

    修改

---
+++ lib/mylib.py
@@ -0,0 +1,7 @@
+#coding:utf-8
+import os
+def get_curdir():
+    return os.getcwd()
+if _name_=="_main_":
+   print(get_curdir())
+   print(os.getcwd())import os
---
+++ lib/var.py
@@ -0,0 +1,4 @@
+#coding:utf-8
+CASE="Case-01"
+MODULE=u"订单模块"
+DATA_ORDER_CASE_03=[["ipad","12"],[u"苹果","23"]]
